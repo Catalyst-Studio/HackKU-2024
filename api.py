fe5d00c1ed249116eb1562907ee6544c814dd219
Lawrence <lawrence.burton@catalyst-studios.cc>
2024-04-13 04:22:39 -0500

    Made changes to API

--- api.py
+++ api.py
@@ -1,17 +1,55 @@
+from fastapi import Depends
 from fastapi.routing import APIRouter
-from status import Status
+import database
+from users import manager
+import datetime
+import uuid
+import api_util
+from models import User
+
+
+def create_new_UUID():
+    return str(uuid.uuid4())
+
 
 api_router = APIRouter(prefix="/api")
-status = Status()
+
 
 @api_router.get("/status")
-async def status():
-    if status.online:
-        return {"status": True}
-    else:
-        return {"status": False}
+async def status_testa(test_a: int = 0):
+    return {"hello world": test_a * 2}
 
 
-@api_router.get("/status/(test_a)")
-async def status_testa(test_a: int):
-    return {"testa": test_a * 2}
+@api_router.get("/dashboard")
+async def dashboard_getAll(user=Depends(manager)):
+    """
+    This function returns the dashboard for the given user.
+
+    Args:
+        user (User): the user for which to retrieve the dashboard
+
+    Returns:
+        dict: a dictionary containing the total hours, recent events, soon events, and affiliate leaderboard for the given user
+    """
+    event_store = []
+    events = database.get_events(user)
+    for event in events:
+        event = dict(event)
+        event.pop("_id")
+        event_store.append(event)
+    total_hours = api_util.total_hours_sort(event_store)
+    recent_events = api_util.most_recent_events(event_store)
+    soon_events = api_util.sorted_future_events(event_store)
+    affiliate_leaderboard = api_util.affiliation_leaderboard(event_store)
+    return {
+        "total_hours": total_hours,
+        "recent_events": recent_events,
+        "soon_events": soon_events,
+        "affiliate_leaderboard": affiliate_leaderboard
+    }
+
+
+@api_router.get("/input")
+async def input_create(location, hours, description, affiliation=None, user=Depends(manager)):
+    database.store_event(user, location, hours, description, affiliation)
+
--- main.py
+++ main.py
@@ -1,23 +1,68 @@
 from urllib import request
 
-from fastapi import FastAPI, Request
+from fastapi import FastAPI, Request, Form
 from fastapi.templating import Jinja2Templates
 from fastapi.staticfiles import StaticFiles
+from fastapi_login import LoginManager
+from starlette.responses import RedirectResponse
+
+import users
+import api
+
 # Making the initial app
 app = FastAPI()
+app.include_router(api.api_router)
 # Adding all the statis files to the webserver
 app.mount("/static", StaticFiles(directory="static"), name="static")
 # Making the templating engine
 templates = Jinja2Templates("templates")
-# Initializing the database
 
 
 # Rendering Pages
 @app.get("/")
+@app.get("/index")
 async def index(request: Request):
     return templates.TemplateResponse("index.html", {"request": request})
 
 
-@app.get("/status/{test_a}")
-async def status_testa(test_a: int):
-    return {"testa": test_a * 2}
+@app.get("/dashboard")
+async def dashboard(request: Request):
+    return templates.TemplateResponse("dashboard.html", {"request": request})
+
+
+@app.get("/input")
+async def input(request: Request):
+    return templates.TemplateResponse("input.html", {"request": request})
+
+
+@app.get("/home")
+async def home(request: Request):
+    return templates.TemplateResponse("home.html", {"request": request})
+
+
+@app.get("/signup")
+async def signup(request: Request):
+    return templates.TemplateResponse("signup.html", {"request": request})
+
+
+@app.post("/signup")
+async def signup_user(request: Request, first_name=Form(), last_name=Form(), email=Form(), password=Form()):
+    users.make_user(first_name, last_name, email, password)
+    token = users.manager.create_access_token(data=dict(sub=email))
+    response = RedirectResponse("/home")
+    users.manager.set_cookie(response, token)
+    return response
+
+
+@app.get("/login")
+async def login(request: Request):
+    return templates.TemplateResponse("login.html", {"request": request})
+
+
+@app.post("/login")
+async def login_user(request: Request, email=Form(), password=Form()):
+    if users.verify_user(email, password):
+        token = users.manager.create_access_token(data=dict(sub=email))
+        response = RedirectResponse("/home")
+        users.manager.set_cookie(response, token)
+        return response
